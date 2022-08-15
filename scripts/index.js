adb7d2ca9f93004763ed4be8325d9c92b74402ed
Oleg Mkhamadiev <mkhamadiev@mail.ru>
2022-08-16 00:22:27 +0500

    первый раз исправлены ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,30 @@
 const popup = document.querySelector('.popup');
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close-button');
-
-const openPopup = function () {
-  popup.classList.add('popup_opened');
-}
-
-const closePopup = function () {
-  popup.classList.remove('popup_opened');
-}
-
-openButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-
 const formElement = document.querySelector('.popup__form');
 const nameInput = document.querySelector('.popup__data');
 const jobInput = document.querySelector('.popup__data');
 
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-  nameInput.getAttribute('value');
-  jobInput.getAttribute('value');
-  const nameInputNew = nameInput.getAttribute('value');
-  const jobInputNew = jobInput.getAttribute('value');
-  nameInput.textContent = nameInputNew;
-  jobInput.textContent = jobInputNew;
+const openPopup = function () {
+  popup.classList.add('popup_opened');
+  nameInput.setAttribute('popup__data', value);
+  jobInput.setAttribute('popup__data', value);
 }
 
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  const profileTitle = document.querySelector('.profile__title');
+  const profileSubtitle = document.querySelector('.profile__subtitle');
+  nameInput.value = profileTitle;
+  jobInput.value = profileSubtitle;
+  nameInput.textContent = profileTitle;
+  jobInput.textContent = profileSubtitle;
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+profileEditButton.addEventListener('click', openPopup);
 formElement.addEventListener('submit', formSubmitHandler);
+popupCloseButton.addEventListener('click', closePopup);
