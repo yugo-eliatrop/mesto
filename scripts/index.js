d2b9662a66f649bc29ae912936287ba7100e29bf
Oleg Mkhamadiev <mkhamadiev@mail.ru>
2022-08-27 21:21:14 +0500

    третий раз исправить ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -2,15 +2,15 @@ const popup = document.querySelector('.popup');
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close-button');
 const formElement = document.querySelector('.popup__form');
-const nameInput = document.querySelector('.popup__data');
-const jobInput = document.querySelector('.popup__data');
+const nameInput = document.querySelector('.popup__input_data_name[name=first-name]');
+const jobInput = document.querySelector('.popup__input_data_name[name=employment]');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 
 const openPopup = function () {
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
   popup.classList.add('popup_opened');
-  nameInput.textContent = nameInput;
-  jobInput.textContent = jobInput;
 }
 
 function formSubmitHandler (evt) {
