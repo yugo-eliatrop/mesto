abd7f66277ba61331321ef3042267f2f2de773d5
Oleg Mkhamadiev <mkhamadiev@mail.ru>
2022-09-15 16:53:56 +0500

    feat: сверстать попап Новое место

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
+
 const popup = document.querySelector('.popup');
+const popupProfile = document.querySelector('.popup__profile');
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popupCloseButton = document.querySelector('.popup__close-button');
 const formElement = document.querySelector('.popup__form');
@@ -6,24 +8,69 @@ const nameInput = document.querySelector('.popup__input_data_name');
 const jobInput = document.querySelector('.popup__input_data_profession');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
+const profileAddButton = document.querySelector('.profile__add-button');
+const popupNewPlaces = document.querySelector('.popup__elements');
+const namePlace = document.querySelector('.popup__input_data_place');
+const linkImage = document.querySelector('.popup__input_link_image');
+const elementsCardText = document.querySelector('.elements__card-text');
+const elementsCardImage = document.querySelector('.elements__card-image');
 
-const openPopup = function () {
+console.log(popup);
+console.log(popupProfile);
+console.log(profileEditButton);
+console.log(popupCloseButton);
+console.log(formElement);
+console.log(nameInput);
+console.log(jobInput);
+console.log(profileTitle);
+console.log(profileSubtitle);
+console.log(profileAddButton);
+console.log(popupNewPlaces);
+console.log(namePlace);
+
+
+function openPopup (popupElement) {
+  popupElement.classList.add('popup_opened');
+}
+
+const openEditPopup = function () {
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileSubtitle.textContent;
-  popup.classList.add('popup_opened');
+  openPopup(popupProfile);
+}
+
+function openAddPopup() {
+  openPopup(popupNewPlaces);
 }
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
-  closePopup();
+  closePopup(popupProfile);
 }
 
-function closePopup() {
-  popup.classList.remove('popup_opened');
+function handleSubmit (evt) {
+  evt.preventDefault();
+  elementsCardText.textContent = namePlace.value;
+  elementsCardImage.textContent = linkImage.value;
+  closePopup(popupNewPlaces);
 }
 
-profileEditButton.addEventListener('click', openPopup);
+function closePopup(popupElement) {
+  popupElement.classList.remove('popup_opened');
+}
+
+profileEditButton.addEventListener('click', openEditPopup);
 formElement.addEventListener('submit', formSubmitHandler);
-popupCloseButton.addEventListener('click', closePopup);
+
+popupCloseButton.addEventListener('click', function () {
+ closePopup(popupProfile);
+})
+
+profileAddButton.addEventListener('click', openAddPopup);
+formElement.addEventListener('submit', handleSubmit);
+
+popupCloseButton.addEventListener('click', function () {
+ closePopup(popupNewPlaces);
+})
